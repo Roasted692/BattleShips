4132ff4b0fee2075fa3881fbc90151f6f9997b6a
Zach Louis <zachlouis@louisza.local>
2021-03-24 13:38:35 -0400

    Trimming things up

--- Battle_Ship_Main.py
+++ Battle_Ship_Main.py
@@ -1,32 +1,49 @@
 import os
 import random
 
-class Initiate_Game:
+
+class Player_Info:
     def __init__(self):
-        self.player = Create_Player()
+        self.player = welcome_create_player()
+        self.player_ship_board = Board(Board.generate_board(10, 10, "O"))
+        self.bot_ship_board = Board(Board.generate_board(10, 10, "O"))
+        self.player_attack_board = Board(Board.generate_board(10, 10, "O"))
+
+class Game:
+    def __init__(self):
+        self.player_info = Player_Info()
+        self.player = self.player_info.player
+
         os.system('cls' if os.name == 'nt' else 'clear')
         print("Setting up new Board...\n")
         print("Legend: \n O = Empty Space \n X = Occupied by Battle Ship \n ")
-        self.board = Board()
-        print(self.board, "\n \n....Enter to continue.")
+        print("-----Player Ship Board-----")
+        print(self.player_info.player_ship_board, "\n \n....Enter to continue.")
         input()
 
 
-
-
-
 class Board(list):
+
+    def generate_board(x: int = 0, y: int = 0, character: str = "O"):
+        '''
+
+        :param x: takes in the number of items you'd like on the X axis of the board.
+        :param y: takes in the number of items you'd like on the Y axis of the board.
+        :param character: takes in the character you'd like to populate the board with.
+        :return:
+        '''
+        return [[character] * x for _ in range(y)]
 
     def __str__(self):
         return "\n".join(" ".join(row) for row in self)
 
 
 def decide_first_turn():
-    return random.randrange(1,3)
+    return random.randrange(1, 3)
 
-
-class Create_Player:
+class welcome_create_player:
     def __init__(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
         self.player_name = input("Welcome to Terminal Battle-Ship, please provide a Player Name:")
         os.system('cls' if os.name == 'nt' else 'clear')
         self.first_turn = "Player" if decide_first_turn() == 1 else "Bot"
@@ -36,14 +53,5 @@ class Create_Player:
         input("\n Press Enter")
 
 
-Initiate_Game()
 
-
-
-
-
-
-
-
-
-
+Game()
--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # Terminal Battle_Ship Sim
+---
 ## Overview
-- Asks player for name & randomly decides if Player & or Bot should go fist.
-- Initiates Game & 10x10 game board/grid.
+---
+- Organized by 3 Main objects
+   ---
+    * Player
+        * Player Name
+        * Player Ship Board
+        * Player Attack Board
+        * Bot Ship Board
+        * Bot Attack Board (Used to store what it's hit so it doesn't attempt to hit it again)
+    ---
+    * Game
+        * Start Game & Gather Player information
+        * Initialize loop to keep player in game until completion
+        * Prompt player to re-play
+            - If yes, restart the game.
+            - If no, end program.
+    ---
+    * Board
+        * Object that instantiates a board to the specifications provided in the list parameter that is fed to it
+    ---    
+
+- To-Do:
+    - Combine remaining functions & classes into 3 classes
+    - Create Game class and begin playing the game with play/bot information on-hand
+    - Create function to restart the game if player chooses to do so
+    
